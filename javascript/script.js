569683d676227f67a80bd9d4bf2ab8beef15d214
SazzadulIslamCr <sazzadul@coderex.co>
2022-04-27 13:05:00 +0600

    uniqueID generate and add to localstorage

--- javascript/script.js
+++ javascript/script.js
@@ -4,26 +4,42 @@ const selectedSev = sev.options[sev.selectedIndex].text;
 const assign = document.getElementById("assign");
 const output = document.getElementById("output");
 
+var details = [];
 
-addBtn.onclick = function () {
-    const key1 = "Description";
-    const decription = desc.value;
+addBtn.onclick = () => {
+    const uId = guid();
+    //console.log(uId);
 
-    const key2 = "Severity";
+    const descKey = "Description";
+    const description = desc.value;
+
+    const sevKey = "Severity";
     const severity = sev.value;
 
-    const key3 = "Assigned";
+    const asKey = "Assigned";
     const assigned = assign.value;
 
-    // console.log(key1);
-    // console.log(decription);
+    let addObj = {
+        id : uId,
+        descKey: description,
+        sevKey: severity,
+        asKey: assigned
+    };
 
-    // console.log(key2);
-    // console.log(severity);
-
-    // console.log(key3);
-    // console.log(assigned);
-
+    details.push(addObj);
     
+    localStorage.setItem('info', JSON.stringify(details));
+
+    //console.log(details);
+
 };
 
+let guid = () => {
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
