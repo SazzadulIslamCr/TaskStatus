e5750bcfaf0fb52879a142d3af210be13574a704
SazzadulIslamCr <sazzadul@coderex.co>
2022-04-27 13:17:18 +0600

    client side validation

--- javascript/script.js
+++ javascript/script.js
@@ -2,35 +2,41 @@ const desc = document.getElementById("desc");
 const sev = document.getElementById("sev");
 const selectedSev = sev.options[sev.selectedIndex].text;
 const assign = document.getElementById("assign");
-const output = document.getElementById("output");
+
+const form = document.getElementById("form");
+
 
 var details = [];
 
 addBtn.onclick = () => {
-    const uId = guid();
-    //console.log(uId);
+    if (desc.value !== "" && assign.value !== ""){
+        const uId = guid();
+        //console.log(uId);
 
-    const descKey = "Description";
-    const description = desc.value;
+        const descKey = "Description";
+        const description = desc.value;
 
-    const sevKey = "Severity";
-    const severity = sev.value;
+        const sevKey = "Severity";
+        const severity = sev.value;
 
-    const asKey = "Assigned";
-    const assigned = assign.value;
+        const asKey = "Assigned";
+        const assigned = assign.value;
 
-    let addObj = {
-        id : uId,
-        descKey: description,
-        sevKey: severity,
-        asKey: assigned
-    };
+        let addObj = {
+            id : uId,
+            descKey: description,
+            sevKey: severity,
+            asKey: assigned
+        };
 
-    details.push(addObj);
+        details.push(addObj);
+        
+        localStorage.setItem('info', JSON.stringify(details));
+    }
+    else {
+        alert("Fill up the required field!!");
+    }
     
-    localStorage.setItem('info', JSON.stringify(details));
-
-    //console.log(details);
 
 };
 
