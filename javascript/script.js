254e03ffa744db605a8d69769ecd064e7831e5e6
SazzadulIslamCr <sazzadul@coderex.co>
2022-04-28 11:40:28 +0600

    showing data on page

--- javascript/script.js
+++ javascript/script.js
@@ -3,15 +3,57 @@ const sev = document.getElementById("sev");
 const selectedSev = sev.options[sev.selectedIndex].text;
 const assign = document.getElementById("assign");
 
-const form = document.getElementById("form");
 
 
 var details = [];
+var data ;
+
+const createCard = (data) => {
+    const container = document.getElementById('showData');
+
+    
+    // Create card element
+    const card = document.createElement('div');
+    card.classList = 'card-body';
+
+    // Construct card content
+    const content = `
+    <div class="card custom_card" id="dataCard">
+        <div class="card-body">
+            <span>Unique ID:</span>
+            <h6 style="text-align:right; bottom: 10px">${data[data.length-1].id}</h6>
+            <h6>Close/Open</h6>
+            <h3 class="card-title">${data[data.length-1].descKey}</h3>
+            <p class="card-text">${data[data.length-1].sevKey}</p>
+            <p class="card-text">${data[data.length-1].asKey}</p>
+            <input class="btn btn-warning" type="button" name="close" id="closeBtn" value="Close">
+            <input class="btn btn-danger" style="margin-left: 1em;"type="button" name="close" id="closeBtn" value="Delete">
+        </div>
+    </div>
+    `;
+
+    // Append newyly created card element to the container
+    container.innerHTML += content;
+    
+}
+
+
+
+
+//generate random unique ID
+let guid = () => {
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
 
 addBtn.onclick = () => {
     if (desc.value !== "" && assign.value !== ""){
         const uId = guid();
-        //console.log(uId);
 
         const descKey = "Description";
         const description = desc.value;
@@ -32,20 +74,24 @@ addBtn.onclick = () => {
         details.push(addObj);
         
         localStorage.setItem('info', JSON.stringify(details));
+
+        data = JSON.parse(localStorage.getItem('info'));
+
+        createCard(data);
+        //sendData(data);
+
+        
     }
     else {
         alert("Fill up the required field!!");
     }
-    
-
 };
+// const sendData = (data) => {
+//     document.getElementById('showData').innerHTML += data[data.length-1].id+"-"
+//         +data[data.length-1].descKey+"-"
+//         +data[data.length-1].asKey+"-"
+//         +data[data.length-1].sevKey+"<br>";
+// }
 
-let guid = () => {
-    let s4 = () => {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-}
+
+        
